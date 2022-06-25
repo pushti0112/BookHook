f9e124ad09d1ecf019295044ad4f2e25ce1dee0a
pushti0112 <71177321+pushti0112@users.noreply.github.com>
2022-06-24 20:36:22 -0400

    login screen ui completed

--- lib/view/LoginScreen.dart
+++ lib/view/LoginScreen.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../global/AppColors.dart';
 
@@ -32,49 +33,159 @@ class _LoginScreenState extends State<LoginScreen> {
         body: Container(
           padding: EdgeInsets.all(16),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Expanded(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                      padding: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: AppColors.light3,
-                      ),
-                      child: Image.asset(
-                        "assets/images/logo1.png",
-                        height: 40,
-                        width: 40,
-                      )),
-                  SizedBox(
-                    height: 24,
-                  ),
-                  Text(
-                    "Signin to unlock the full experience",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: AppColors.dark2,
-                      fontSize: 16,
+                child: ListView(
+                  shrinkWrap: true,
+                  // crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.light3,
+                        ),
+                        child: Image.asset(
+                          "assets/images/logo1.png",
+                          height: 40,
+                          width: 40,
+                        )),
+                    SizedBox(
+                      height: 24,
                     ),
-                  ),
-                  Expanded(
-                      child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 24,
+                    Center(
+                      child: Text(
+                        "Sign In to unlock the full experience",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: AppColors.dark2,
+                          fontSize: 16,
+                        ),
                       ),
-                    ],
-                  )),
-                ],
-              ))
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    TextFormField("Username or Email Id"),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField("Password"),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        // ignore: sort_child_properties_last
+                        child: Text(
+                          "Forgot Password ?",
+                          style: TextStyle(color: AppColors.primary),
+                        ),
+                        onPressed: () async {},
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Container(
+                        width: double.maxFinite,
+                        child: ElevatedButton(
+                          child: Text("Sign In"),
+                          style: ElevatedButton.styleFrom(
+                              primary: AppColors.primary,
+                              padding: EdgeInsets.all(16)),
+                          onPressed: () {},
+                        )),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Row(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Colors.black54,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Text(
+                          "OR",
+                          style: TextStyle(color: Colors.black54),
+                        ),
+                        SizedBox(
+                          width: 6,
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    OutlinedButton(
+                        // ignore: sort_child_properties_last
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 12),
+                              child: SvgPicture.asset(
+                                "assets/images/google.svg",
+                                height: 24,
+                                width: 24,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 16,
+                            ),
+                            Text(
+                              "Continue with Google",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                          ],
+                        ),
+                        style: OutlinedButton.styleFrom(
+                          backgroundColor: AppColors.light4,
+                          side: BorderSide(color: Colors.black45, width: 0.5),
+                        ),
+                        onPressed: () => {}),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
       )),
+    );
+  }
+
+  Widget TextFormField(String hint) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        border: Border.all(color: Colors.white),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black45, width: 0.5),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black, width: 1),
+          ),
+          hintText: hint,
+        ),
+      ),
     );
   }
 }
