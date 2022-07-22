4b3586c11110a2147a8fec7e12e13726e3de41c6
Ashutosh Fichadia <vkra.fichadia@gmail.com>
2022-07-22 01:33:54 -0400

    Test MSG

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:sizer/sizer.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
+//main
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
